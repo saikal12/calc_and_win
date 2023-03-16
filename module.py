427bc6714de4297661bd9cc85213ab4789515e1f
Сулайманова Сайкал <sulaymanovasaykal2003gmail.com>
2023-03-16 13:03:30 +0300

    debug

--- module.py
+++ module.py
@@ -36,15 +36,16 @@ def get_user_attack():
         input_attack = input('Введи тип атаки: ').lower()
         attack_value = attacks_types[input_attack]()
         print(f'Количество очков твоей атаки: {attack_value}.')
-        total += 1
+        total += attack_value
     return total
 
 
 def run_game():
     user_total_attack = get_user_attack()
     enemy_health = set_enemy_health()
+    enemy_health_after = abs(enemy_health - user_total_attack)
     print(f'Тобой нанесён урон противнику равный {user_total_attack}.')
-    print(f'Очки здоровья противника после твоей атаки: {enemy_health}.')
+    print(f'Очки здоровья противника после твоей атаки: {enemy_health_after}')
     if compare_valumes(enemy_health, user_total_attack):
         print('Ура! Победа за тобой!')
     else:
@@ -52,6 +53,8 @@ def run_game():
     yes_no = {
         'Y': True,
         'N': False,
+        'n': False,
+        'y': True,
     }
     replay = input('Чтобы сыграть ещё раз, введи "y"; '
                    'если не хочешь продолжать игру, введи "n": ')
